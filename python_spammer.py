49958eb59bffbfa3de9a8bc7ff7cbd7adde03b7b
Michael-ML7 <79148018+Michael-ML7@users.noreply.github.com>
2022-03-27 00:42:43 +0800

    Update python_spammer.py

--- python_spammer.py
+++ python_spammer.py
@@ -4,7 +4,7 @@
 import pyautogui, time
 def SendMessage():
     time.sleep(5)
-    text = open("text.txt")
+    text = open("text.txt") # to be created and this will be your text
     for each_line in text:
         pyautogui.typewrite(each_line)
         pyautogui.press('enter')
